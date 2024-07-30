4d1297bcfa93899ccb5ebccd2328d64f40c19457
Nerinho Nascimento <neramornas@gmail.com>
2024-07-29 23:57:41 -0300

    initial commit, refatoracao aplication.yml e add swagger no pom.xml
    Signed-off-by: Nerinho Nascimento <neramornas@gmail.com>

---
+++ src/main/java/edu/sisreserva/cia/SistemaDeReservaApplication.java
@@ -0,0 +1,13 @@
+package edu.sisreserva.cia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/sisreserva/cia/SistemaDeReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sisreserva.cia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
